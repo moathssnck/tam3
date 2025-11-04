88f61d0c4a993168398860b8ac8cbf45e83c8017
moathssnck <myneww00@gmail.com>
2025-11-04 19:17:54 +0300

    Nz

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import type React from "react";
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-10860808864');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
